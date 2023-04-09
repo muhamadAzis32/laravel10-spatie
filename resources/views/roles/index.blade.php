4505c83c6d764f76b0bd96bb8f946e20d79e38b4
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2023-04-09 12:53:11 +0700

    Menambahkan proses menambahkan role

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -7,9 +7,10 @@
                 <h2>Role Management</h2>
             </div>
             <div class="pull-right">
-                @can('role-create')
+                {{-- @can('role-create')
                     <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
-                @endcan
+                @endcan --}}
+                <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
             </div>
         </div>
     </div>
@@ -35,14 +36,18 @@
                 <td>{{ $role->name }}</td>
                 <td>
                     <a class="btn btn-info" href="{{ route('roles.show', $role->id) }}">Show</a>
-                    @can('role-edit')
+                    <a class="btn btn-primary" href="{{ route('roles.edit', $role->id) }}">Edit</a>
+                    {{-- @can('role-edit')
                         <a class="btn btn-primary" href="{{ route('roles.edit', $role->id) }}">Edit</a>
-                    @endcan
-                    @can('role-delete')
+                    @endcan --}}
+                    {{-- @can('role-delete')
                         {!! Form::open(['method' => 'DELETE', 'route' => ['roles.destroy', $role->id], 'style' => 'display:inline']) !!}
                         {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
                         {!! Form::close() !!}
-                    @endcan
+                    @endcan --}}
+                    {!! Form::open(['method' => 'DELETE', 'route' => ['roles.destroy', $role->id], 'style' => 'display:inline']) !!}
+                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                    {!! Form::close() !!}
                 </td>
             </tr>
         @endforeach
