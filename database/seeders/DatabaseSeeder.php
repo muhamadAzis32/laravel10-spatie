394d15848b7716a36b3007bdb1b3e406121ca1b7
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2023-04-09 13:01:31 +0700

    Menambahkan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            CreateAdminUserSeeder::class,
+            PermissionTableSeeder::class,
+        ]);
     }
 }
